aa8428c195b0b9467a1e4c9eb64907b314e4d55e
Davi Paulino <davipm@hotmail.com>
2022-08-18 16:02:56 -0300

    Achando o maior e o menor com e sem métodos Integer

--- ProblemasPraResolver.java
+++ ProblemasPraResolver.java
@@ -1,39 +1,60 @@
 package TestingThings;
 
+import java.util.Scanner;
+
 public class ProblemasPraResolver {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// find out how to get the bigger and the smaller number of a sequence with
+		// "Integer.MAX_VALUE" and "Integer.MIN_VALUE"
+
+		int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+
+		System.out.println("Com \"Integer.MAX_VALUE\" e \"Integer.MIN_VALUE\"");
+		System.out.println("Sequência de 5 números");
+		for (int i = 0; i < 5; i++) {
+			int num = sc.nextInt();
+			if (num < min) {
+				min = num;
+			}
+			if (num > max) {
+				max = num;
+			}
+
+		}
+		System.out.println("Menor: " + min);
+		System.out.println("Maior: " + max);
+
+		// find out how to get the bigger and the smaller number of a sequence without
+		// "Integer.MAX_VALUE" and "Integer.MIN_VALUE"
+		Integer maior = null, menor = null;
+		Integer CONSTANTE = null;
 		
-	//found out how to get the bigger and the smaller number of a sequence, with "Integer.MAX_VALUE" and without it	
-	//achar o porcentual de impares e pares em uma quantidade indeterminada de valores
-	}
-	
-	
-	static int soma(int[] valor) {
-		int soma=0;
-		for(int valores:valor) {
-			soma+=valores;
+		System.out.println("\nSem \"Integer.MAX_VALUE\" e \"Integer.MIN_VALUE\"");
+		System.out.println("Sequência de 5 números");
+		for (int i = 0; i < 5; i++) {
+			int num = sc.nextInt();
+
+			if (CONSTANTE == null) {
+				CONSTANTE = num;
+				maior = CONSTANTE;
+				menor = CONSTANTE;
+			}
+
+			if (num > maior) {
+				maior = num;
+			}
+			if (num < menor) {
+				menor = num;
+			}
+
 		}
-		return soma;
+		System.out.println("Menor: " + menor);
+		System.out.println("Maior: " + maior);
+
+		sc.close();
 	}
-	
-	static int soma(Integer... valor) {
-		int soma=0;
-		for(int valores:valor) {
-			soma+=valores;
-		}
-		return soma;
-	}
-	
-	//make it work like the other ones
-	
-	/*static Integer somaArray(ArrayList valor) {
-		int soma=0;
-		for(int i=0;i<valor;i++) {
-			
-		}
-		
-		return soma;
-	}*/
-	
+
 }
