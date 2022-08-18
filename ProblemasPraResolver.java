224177c47e9613214993fc57103f8e032b464c09
Davi Paulino <davipm@hotmail.com>
2022-08-18 16:36:42 -0300

    achar o porcentual de uma quantidade indeterminadaa de par/impar

--- ProblemasPraResolver.java
+++ ProblemasPraResolver.java
@@ -1,5 +1,6 @@
 package TestingThings;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class ProblemasPraResolver {
@@ -7,52 +8,28 @@ public class ProblemasPraResolver {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		// find out how to get the bigger and the smaller number of a sequence with
-		// "Integer.MAX_VALUE" and "Integer.MIN_VALUE"
-
-		int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
-
-		System.out.println("Com \"Integer.MAX_VALUE\" e \"Integer.MIN_VALUE\"");
-		System.out.println("Sequência de 5 números");
-		for (int i = 0; i < 5; i++) {
-			int num = sc.nextInt();
-			if (num < min) {
-				min = num;
-			}
-			if (num > max) {
-				max = num;
-			}
-
-		}
-		System.out.println("Menor: " + min);
-		System.out.println("Maior: " + max);
-
-		// find out how to get the bigger and the smaller number of a sequence without
-		// "Integer.MAX_VALUE" and "Integer.MIN_VALUE"
-		Integer maior = null, menor = null;
-		Integer CONSTANTE = null;
+		boolean loop=true;
+		int par=0,impar=0;
 		
-		System.out.println("\nSem \"Integer.MAX_VALUE\" e \"Integer.MIN_VALUE\"");
-		System.out.println("Sequência de 5 números");
-		for (int i = 0; i < 5; i++) {
-			int num = sc.nextInt();
-
-			if (CONSTANTE == null) {
-				CONSTANTE = num;
-				maior = CONSTANTE;
-				menor = CONSTANTE;
+		//achar o porcentual de impares e pares em uma quantidade indeterminada de valores
+		while(loop==true) {
+			int num=sc.nextInt();
+			
+			if(num%2==0) {
+				par++;
+			}else {
+				impar++;
 			}
-
-			if (num > maior) {
-				maior = num;
+			
+			System.out.println("Mais um número? [s]/[n]");
+			String resp=sc.next();
+			if(resp.equalsIgnoreCase("n")) {
+				break;
 			}
-			if (num < menor) {
-				menor = num;
-			}
-
 		}
-		System.out.println("Menor: " + menor);
-		System.out.println("Maior: " + maior);
+		
+		System.out.println("Porcentagem Par: "+(double)(par*100)/(par+impar)+"%");
+		System.out.println("Porcentagem Ímpar: "+(double)(impar*100)/(par+impar)+"%");
 
 		sc.close();
 	}
